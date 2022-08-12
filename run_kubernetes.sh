cdf8be3747c09c9ba144b9457e2dd90f06dfb583
jurexapp <mgbechetajustice@gmail.com>
2022-08-12 11:34:30 +0100

    completed support for kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,10 +8,8 @@
 dockerpath="bethanecol/mlop:latest"
 # Step 2
 # Run the Docker Hub container with kubernetes
-# kubectl create deploy  mlop-v1 --image=$dockerpath 
-kubectl run mlop-v1\
-    --image=$dockerpath\
-    --port=80 --labels app=mlop-v1
+kubectl create deploy python-helloworld --image=$dockerpath
+
 # --generator=run-pod/v1
 
 # Step 3:
@@ -21,6 +19,6 @@ kubectl get  pods
 # Forward the container port to a host
 # kubectl port-forward pod/mlop-proj.v1 8000:80
 # kubectl expose pod mlop-v1 --port=8080 --target-port=8080 --name mlop-service
-kubectl port-forward mlop-v1 8000:80
+kubectl port-forward pod/python-helloworld-6ccd8846d8-pxp9c --address 0.0.0.0 80:80
 
 
